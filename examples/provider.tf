50314a7a678d30a97d8ff80eddfdd8658c01c1ce
nico.pro <nico.pro@comp-host.com>
2023-08-16 13:32:54 +0200

    updated code gen

--- examples/provider.tf
+++ examples/provider.tf
@@ -12,22 +12,22 @@ provider "mssql" {
 
 resource "mssql_aad_login_v2" "fun" {
   login_name = "sida.epi@comp-host.com"
-  server     = {
-#    sql_login = {
-#      username = "nico"
-#
-#      password = "Qi9PTbKwebW8XOfKZvyvfeXJu7QZq8HwG4LER7QgY7gzGnXaBQmtKdpwZ7a4XkD7"
-#    }
+  server = {
+    #    sql_login = {
+    #      username = "nico"
+    #
+    #      password = "Qi9PTbKwebW8XOfKZvyvfeXJu7QZq8HwG4LER7QgY7gzGnXaBQmtKdpwZ7a4XkD7"
+    #    }
     "azure_cli" = {
 
     }
-#
-#    azure_login = {
-#      tenant_id = "fun"
-#
-#      client_id = "fun"
-#      client_secret = "fun"
-#    }
+    #
+    #    azure_login = {
+    #      tenant_id = "fun"
+    #
+    #      client_id = "fun"
+    #      client_secret = "fun"
+    #    }
     host = "nico-pro-2.database.windows.net"
   }
 }
